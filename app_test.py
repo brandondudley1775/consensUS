91ef4d7f10f59ff56028ffc003804e971da6ddb2
brandondudley1775 <brandondudley1775@gmail.com>
2019-03-07 15:13:59 -0500

    Add files via upload

---
+++ app_test.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/login')
+def log_in():
+	return 'Login page!'
+
+@app.route('/validate_identity')
+def log_in():
+	return 'Identity page!'
